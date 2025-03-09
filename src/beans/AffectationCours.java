fcf03d523ba688692a4c4426ee4d7157c07300ec
Zineb2004-beep <bidaszineb4@gmail.com>
2025-03-09 16:30:11 +0000

    Add files via upload

---
+++ src/beans/AffectationCours.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author User
+ */
+public class AffectationCours {
+
+    private Professeur prof;
+    private Cour cour;
+    
+    public AffectationCours(Professeur prof, Cour cour) {
+        this.prof = prof;
+        this.cour = cour;
+    }
+        
+    
+    public Professeur getProf() {
+        return prof;
+    }
+
+    public void setProf(Professeur prof) {
+        this.prof = prof;
+    }
+
+    public Cour getCour() {
+        return cour;
+    }
+
+    public void setCour(Cour cour) {
+        this.cour = cour;
+    }
+
+}
