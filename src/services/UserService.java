a4ccda9636108306e4cf938fc98dfe012a0c9894
Bidas Zineb <bidaszineb4@gmail.com>
2025-03-19 23:56:19 +0000

    Add files via upload

---
+++ src/services/UserService.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import beans.User;
+import connexion.Connexion;
+import dao.IDaoUser;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
+
+/**
+ *
+ * @author User
+ */
+public class UserService implements IDaoUser {
+
+    private Connexion connexion;
+
+    public UserService() {
+        connexion = Connexion.getInstance();
+    }
+
+    @Override
+    public boolean addUser(User user) {
+        String req = "INSERT INTO user (login, password) VALUES (?, SHA1(?))";
+        try {
+            PreparedStatement ps = connexion.getCn().prepareStatement(req);
+            ps.setString(1, user.getLogin());
+            ps.setString(2, user.getPassword());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public User findUserByLogin(String login) {
+        String req = "SELECT * FROM user WHERE login = ?";
+        try {
+            PreparedStatement ps = connexion.getCn().prepareStatement(req);
+            ps.setString(1, login);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new User(rs.getString("login"), rs.getString("password"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public boolean authentificate(String login, String password) {
+        String req = "SELECT * FROM user WHERE login = ? AND password = SHA1(?)";
+        try {
+            PreparedStatement ps = connexion.getCn().prepareStatement(req);
+            ps.setString(1, login);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    public String resetPassword(String login) {
+        String newPassword = generateTemporaryPassword();
+        String query = "UPDATE user SET password = SHA1(?) WHERE login = ?";
+
+        try {
+            PreparedStatement pstmt = connexion.getCn().prepareStatement(query);
+            pstmt.setString(1, newPassword);
+            pstmt.setString(2, login);
+
+            int rowsUpdated = pstmt.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                return newPassword; 
+            } else {
+                return null; 
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la réinitialisation du mot de passe : " + e.getMessage());
+
+        }
+        return null;
+    }
+
+    private String generateTemporaryPassword() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(8);
+        for (int i = 0; i < 8; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+}
